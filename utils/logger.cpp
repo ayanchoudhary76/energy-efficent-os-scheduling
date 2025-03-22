08e95f09494f87e696b4ba5aab1e565098436441
ayanchoudhary76 <ayanchoudhary76@gmail.com>
2025-03-22 23:59:44 +0530

    Improved logging with time stamp

--- utils/logger.cpp
+++ utils/logger.cpp
@@ -1,15 +1,24 @@
 #include "logger.h"
-#include <fstream>
-#include <iostream>
+#include <ctime>
+
+std::string getCurrentTime()
+{
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    char buffer[20];
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", ltm);
+    return std::string(buffer);
+}
 
 void Logger::log(const std::string message)
 {
-    std::cout << "[LOG] " << message << std::endl;
+    std::string timestamp = getCurrentTime();
+    std::cout << "[LOG " << timestamp << "] " << message << std::endl;
 
     std::ofstream logFile("log.txt", std::ios::app);
     if (logFile.is_open())
     {
-        logFile << "[LOG] " << message << std::endl;
+        logFile << "[LOG " << timestamp << "] " << message << std::endl;
         logFile.close();
     }
     else
