d4e777b894e04566ec3e3dcb42cd2f6d8ca334c6
ayanchoudhary76 <ayanchoudhary76@gmail.com>
2025-03-22 23:57:54 +0530

    Implemented preemptive SJF scheduling

--- modules/scheduler.cpp
+++ modules/scheduler.cpp
@@ -1,86 +1,54 @@
 #include "scheduler.h"
 #include "../utils/logger.h"
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <queue>
+#include <algorithm>
 
 void SJFScheduler::schedule(std::vector<Process> &processes)
 {
+    auto cmp = [](const Process &a, const Process &b)
+    { return a.burst_time > b.burst_time; };
+    std::priority_queue<Process, std::vector<Process>, decltype(cmp)> pq(cmp);
+
+    int current_time = 0, total_energy = 0;
+    int index = 0, n = processes.size();
+
     std::sort(processes.begin(), processes.end(), [](Process a, Process b)
-              {
-        if (a.arrival_time == b.arrival_time)
-            return a.burst_time < b.burst_time;
-        return a.arrival_time < b.arrival_time; });
+              { return a.arrival_time < b.arrival_time; });
 
-    Logger::log("Starting SJF Scheduling...");
-    int current_time = 0;
-    int total_energy = 0;
-    int total_tat = 0;
-    int total_wt = 0;
-    int n = processes.size();
+    Logger::log("Starting Preemptive SJF Scheduling...");
 
-    std::vector<std::pair<int, int>> gantt_chart;
-
-    std::cout << "\nProcess Execution Order:\n";
-    std::cout << "-------------------------------------------------------------------\n";
-    std::cout << "| Process | Arrival Time | Burst Time | Completion Time | TAT | WT |\n";
-    std::cout << "-------------------------------------------------------------------\n";
-
-    for (auto &p : processes)
+    while (index < n || !pq.empty())
     {
-        if (current_time < p.arrival_time)
+        while (index < n && processes[index].arrival_time <= current_time)
         {
-            current_time = p.arrival_time;
+            pq.push(processes[index]);
+            index++;
         }
 
-        int completion_time = current_time + p.burst_time;
-        int tat = completion_time - p.arrival_time;
-        int wt = tat - p.burst_time;
+        if (!pq.empty())
+        {
+            Process p = pq.top();
+            pq.pop();
+            std::cout << "Executing Process P" << p.id << " at time " << current_time << "\n";
+            Logger::log("Executing Process P" + std::to_string(p.id) + " at time " + std::to_string(current_time));
 
-        total_tat += tat;
-        total_wt += wt;
-        int energy_used = p.burst_time * 5;
-        total_energy += energy_used;
+            p.burst_time--;
+            total_energy += 5;
 
-        gantt_chart.push_back({p.id, current_time});
+            if (p.burst_time > 0)
+            {
+                pq.push(p);
+            }
 
-        std::cout << "|    P" << p.id << "    |      " << p.arrival_time
-                  << "       |     " << p.burst_time
-                  << "     |       " << completion_time
-                  << "       |   " << tat
-                  << "  |  " << wt << "  |\n";
-
-        Logger::log("Process " + std::to_string(p.id) + " executed, CT: " +
-                    std::to_string(completion_time) + ", TAT: " +
-                    std::to_string(tat) + ", WT: " + std::to_string(wt) +
-                    ", Energy Used: " + std::to_string(energy_used));
-
-        current_time = completion_time;
+            current_time++;
+        }
+        else
+        {
+            current_time++;
+        }
     }
-
-    float avg_tat = (float)total_tat / n;
-    float avg_wt = (float)total_wt / n;
-
-    std::cout << "-------------------------------------------------------------------\n";
-    std::cout << "Total Energy Consumption: " << total_energy << " units\n";
-    std::cout << "Average Turnaround Time: " << avg_tat << "\n";
-    std::cout << "Average Waiting Time: " << avg_wt << "\n";
 
     Logger::log("Total Energy Consumption: " + std::to_string(total_energy) + " units");
-    Logger::log("Average Turnaround Time: " + std::to_string(avg_tat));
-    Logger::log("Average Waiting Time: " + std::to_string(avg_wt));
-
-    std::cout << "\nGantt Chart:\n";
-    std::cout << "|";
-    for (const auto &entry : gantt_chart)
-    {
-        std::cout << " P" << entry.first << " |";
-    }
-    std::cout << "\n";
-
-    for (const auto &entry : gantt_chart)
-    {
-        std::cout << entry.second << "    ";
-    }
-    std::cout << current_time << "\n";
 }
